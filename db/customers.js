4278d90b74cfa68beddb15a747b188d3a91a3682
OlSavMe <olga_savka.os@mail.ru>
2021-01-23 01:54:30 +0200

    mocha, chai and chai-http instaled for testing, in db/customers fuction added to deleted all customers before testing

--- db/customers.js
+++ db/customers.js
@@ -92,10 +92,20 @@ const updateCustomer = (req, res) => {
   res.json(editedCustomer);
 };
 
+// Delete all customers before testing
+const deleteAllCustomers = () => {
+  db.query("DELETE FROM customers", (err, res) => {
+    if (err) {
+      return console.error("Error executing query", err.stack);
+    }
+  });
+};
+
 module.exports = {
   getAllCustomers: getAllCustomers,
   getCustomerById: getCustomerById,
   addCustomer: addCustomer,
   deleteCustomer: deleteCustomer,
   updateCustomer: updateCustomer,
+  deleteAllCustomers: deleteAllCustomers,
 };
