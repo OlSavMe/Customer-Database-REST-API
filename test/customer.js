22046a36d0e69b1f887869dcb69a51966d936869
OlSavMe <olga_savka.os@mail.ru>
2021-01-24 15:02:14 +0200

    comments added to adding customer test

--- test/customer.js
+++ test/customer.js
@@ -4,8 +4,10 @@ const app = require("../index");
 const query = require("../db/customers");
 const should = chai.should();
 
+// using chai-http plugin with use()function of chai library
 chai.use(chaihttp);
 
+// defining a testing customer object
 const testCustomer = {
   firstname: "new",
   lastname: "Updateman",
@@ -13,6 +15,7 @@ const testCustomer = {
   phone: "197600",
 };
 
+// sending POST request to api/customers endpoint with chai-http request() function
 describe("/POST customers", () => {
   beforeEach((done) => {
     query.deleteAllCustomers();
@@ -26,24 +29,11 @@ describe("/POST customers", () => {
       .set("Content-Type", "application/json")
       .send(JSON.stringify(testCustomer))
       .end((err, res) => {
+        // checking the test results with should assertions
         res.should.have.status(200);
         res.body.should.be.a("object");
         res.body.should.have.property("firstname");
-        done();
+        done(); //  the callback is completed
       });
   });
 });
-
-// describe("/GET customers", () => {
-//   it("Fetch all customers", (done) => {
-//     chai
-//       .request(app)
-//       .get("/api/customers")
-//       .end((err, res) => {
-//         res.should.have.status(200);
-//         res.body.should.be.a("array");
-//         res.body.length.should.be.eql(1);
-//         done();
-//       });
-//   });
-// });
