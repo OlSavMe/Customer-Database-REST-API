a39396f2337388db6b8d79a59d99a737d6c0570e
OlSavMe <olga_savka.os@mail.ru>
2021-01-25 18:11:28 +0200

    authentication with JSON Web Token added

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const query = require("./db/customers");
+const auth = require("./services/authenticate");
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
 
-app.get("/api/customers", query.getAllCustomers);
-app.get("/api/customers/:id", query.getCustomerById);
-app.post("/api/customers", query.addCustomer);
-app.delete("/api/customers/:id", query.deleteCustomer);
-app.put("/api/customers/:id", query.updateCustomer);
+app.get("/api/customers", auth.authenticate, query.getAllCustomers);
+app.get("/api/customers/:id", auth.authenticate, query.getCustomerById);
+app.post("/api/customers", auth.authenticate, query.addCustomer);
+app.delete("/api/customers/:id", auth.authenticate, query.deleteCustomer);
+app.put("/api/customers/:id", auth.authenticate, query.updateCustomer);
+// Route for login
+app.post("/login", auth.login);
 
 app.listen(port, () => {
-  {
-  }
   console.log(`Server is running on port ${port}.`);
 });
 
